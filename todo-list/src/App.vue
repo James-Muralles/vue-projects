0201605050e42249be6b786218a54d4f5830fe55
James Muralles <james.muralles23@gmail.com>
2022-05-21 20:51:37 -0500

    completed todo list

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -1,29 +1,32 @@
 <template>
   <div id="app" class="container">
     <h1 class="text-center">Todo App</h1>
-
-
     <form @submit.prevent="addTodo()">
       <div class="mb-3">
         <label for="newTodo" class="form-label">New Todo</label>
         <input v-model="newTodo" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="newTodoHelp"
           placeholder="Walk the dog">
-        <div id="newTodoHelp" class="form-text">Enter a new todo</div>
+        <small id="newTodoHelp" class="form-text">Enter a new todo</small>
       </div>
-
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <button type="button" class="btn btn-primary">Submit</button>
     </form>
-
-
     <ul class="list-group mt-5">
       <li class="list-group-item" v-for="todo in todos">
-      <button type="button" class="btn btn-primary">DONE</button>
-      {{ todo }}
+        <button v-if="!todo.done" 
+        @click="markDone(todo)" 
+        type="button" 
+        class="btn btn-primary">
+          DONE</button>
+        <button 
+        type="button" 
+        class="btn btn-danger" 
+        @click="removeTodo(todo)">
+          DELETE</button>
+        <span :class="{
+          isDone: todo.done
+        }">{{ todo.title }}</span>
       </li>
-      
     </ul>
-
-
   </div>
 </template>
 
@@ -39,6 +42,25 @@ export default {
 
   }),
 
+  watch: {
+
+    todos: {
+      handler() {
+        localStorage.todos = JSON.stringify(this.todos)
+      },
+    deep: true
+
+    },
+  },
+
+  mounted() {
+    if (localStorage.todos) {
+      this.todos = JSON.parse(localStorage.todos)
+    }
+  },
+
+
+
   methods: {
     addTodo() {
       this.todos.push({
@@ -47,13 +69,25 @@ export default {
       });
       this.newTodo = '';
       console.log(this.todos);
+    },
+    markDone(todo) {
+      todo.done = true;
+      // console.log();
+    },
+    removeTodo(todo) {
+      this.todos.splice(this.todos.indexOf(todo), 1);
     }
   },
 
 
 }
 
+
 </script>
 
 <style>
+.isDone {
+  text-decoration: line-through;
+  text-decoration-color: red;
+}
 </style>
